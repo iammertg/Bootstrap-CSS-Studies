be36805dadd0384c0a15d9ed4cccf4928d443d66
Mert Gozukara <mert.gozukara@adesso.com.tr>
2019-03-09 23:57:22 +0300

    Content added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild("sideBar") sideBar: ElementRef
   title = 'Flexlayout';
+
+  sideBarIsActive: boolean = true;
+
+  ngOnInit() {
+
+  }
+
+  onClickSideBar() {
+    this.sideBarIsActive = false;
+  }
+
+  onClickContentHeader() {
+    this.sideBarIsActive= true;
+  }
 }
